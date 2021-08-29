d8eb1b04334c125b9be6cc61f007c07f7da0a0f4
SANDEEP14380 <77968338+SANDEEP14380@users.noreply.github.com>
2021-08-29 17:42:52 +0100

    Update main.bat

--- src/main.bat
+++ src/main.bat
@@ -1,2 +1,2 @@
-C:\Users\AH05350\Anaconda3\python.exe "C:\Users\AH05350\Documents\self\documents\Umesh\API_fetch_smarthome\energy_dashboard-master\python\main_fetch.py"
+C:\Users\AH05350\Anaconda3\python.exe "C:\\Users\sande\Desktop\smart\SmartHomeNotifier\src\main_fetch.py"
 pause
